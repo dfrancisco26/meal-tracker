5699af48479e307fdd3ecd2fa501214cc6f49ab2
David Francisco <dfrancisco2677@gmail.com>
2022-05-03 14:31:41 -0700

    Form submission works for ingredient array.

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions and grab DOM elements
-const addIngredient = document.getElementById('add-ingredient');
+const addForm = document.getElementById('add-ingredient');
 // let state
-
+let ingredients = [];
 // set event listeners 
+addForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(addForm);
+    const item = {
+        ingredient: data.get('ingredient'),
+        qty: data.get('qty'),
+        measurement: data.get('measurement'),
+    };
+    ingredients.push(item);
+    console.log(ingredients);
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
